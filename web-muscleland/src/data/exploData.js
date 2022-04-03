7bb838a43bd726001d9aad4c831fb090d0e1425c
waratpala <97785199+waratpala@users.noreply.github.com>
2022-04-03 19:31:44 +0700

    mission

--- web-muscleland/src/data/exploData.js
+++ web-muscleland/src/data/exploData.js
@@ -15,16 +15,4 @@ export const dataDistance = [
     Today: 50,
     Km: 30,
   },
-  {
-    Today: 40,
-    Km: 40,
-  },
-  {
-    Today: 50,
-    Km: 60,
-  },
-  {
-    Today: 30,
-    Km: 70,
-  },
 ];
